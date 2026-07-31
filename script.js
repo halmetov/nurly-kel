29d25fea77ad2701268974ee57a1b7d18b61a71b
halmetov <ithubsayramm@gmail.com>
2026-07-31 20:23:00 +0500

    asd

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 const btn = document.getElementById('themeToggle');
 const root = document.documentElement;
 
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  root.setAttribute('data-theme', 'dark');
-  btn.textContent = 'Light';
-}
-
 btn.addEventListener('click', () => {
   const next = root.getAttribute('data-theme') === 'light' ? 'dark' : 'light';
   root.setAttribute('data-theme', next);
